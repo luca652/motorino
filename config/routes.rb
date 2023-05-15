2280dfcae55af6a5738c47f95cd65bcc451a3277
luca gastaldi <lucagastaldi82@gmail.com>
2023-05-15 21:21:33 +0100

    added bookings controller and routes (create, my_bookings)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-
   devise_for :users
   root to: "pages#index"
 
-  resources :mopeds
+  resources :mopeds do
+    resources :bookings, only: [:create]
+  end
+  get "my_bookings", to: "bookings#my_bookings"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
