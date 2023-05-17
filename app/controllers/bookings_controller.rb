3ce2f2fdf8b3b80d328e59626260c21cf8a94327
luca gastaldi <lucagastaldi82@gmail.com>
2023-05-17 15:03:54 +0100

    added name and price to bookings page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,7 +3,7 @@ class BookingsController < ApplicationController
     @my_bookings = Booking.where(user: current_user)
   end
 
-  # def my_requests
-  #   @my_requests = Booking.joins(:moped).where(user: current_user)
-  # end
+  def my_requests
+    @my_requests = Booking.joins(:moped).where(user: current_user)
+  end
 end
