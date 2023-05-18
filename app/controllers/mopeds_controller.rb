4f0c78468ab838adf2fd4e7b438d2192a04d8d9b
luca gastaldi <lucagastaldi82@gmail.com>
2023-05-18 13:06:50 +0100

    added new and create methods to mopeds controller - also added simple form to 'mopeds/new' page

--- app/controllers/mopeds_controller.rb
+++ app/controllers/mopeds_controller.rb
@@ -6,4 +6,19 @@ class MopedsController < ApplicationController
   def new
     @moped = Moped.new
   end
+
+  def create
+    @moped = Moped.new(moped_params)
+    if @moped.save
+      redirect_to moped_path(@moped)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def moped_params
+    params.require(:moped).permit(:name, :color, :description, :price, :n_of_passengers)
+  end
 end
