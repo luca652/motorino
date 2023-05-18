8754c8a0f475643e775643e6d1aa3465c2bcdda8
luca gastaldi <lucagastaldi82@gmail.com>
2023-05-18 21:32:51 +0100

    added booking form on show page and create method in bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,29 @@
 class BookingsController < ApplicationController
+
+  def new
+    @moped = Moped.find(params[:moped_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @moped = Moped.find(params[:moped_id])
+    @booking = Booking.new(booking_params)
+    @booking.moped = @moped
+    if @booking.save
+      redirect_to my_bookings_path(current_user)
+    else
+      render "show"
+    end
+  end
+
   def my_bookings
     @my_bookings = Booking.where(user: current_user)
     @my_mopeds = Moped.where(user: current_user)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :moped_id, :start_date, :end_date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#index"
 
   resources :mopeds do
-    resources :bookings, only: [:create]
+    resources :bookings, only: [:create, :new]
   end
   get "my_bookings", to: "bookings#my_bookings"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
