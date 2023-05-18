2861d67d7076735244ffe5c16f74b0684e95c02a
luca gastaldi <lucagastaldi82@gmail.com>
2023-05-18 10:15:20 +0100

    working on displaying booking requests

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,8 +2,4 @@ class BookingsController < ApplicationController
   def my_bookings
     @my_bookings = Booking.where(user: current_user)
   end
-
-  def my_requests
-    @my_requests = Booking.joins(:moped).where(user: current_user)
-  end
 end
