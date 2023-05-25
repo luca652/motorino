60c299b376492b72a28dec4f4636027267cbcd43
zak freifer <freifery@gmail.com>
2023-05-25 20:07:09 +0100

    show page upgrade

--- db/schema.rb
+++ db/schema.rb
@@ -87,7 +87,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_24_134644) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "bookings", "mopeds", on_delete: :cascade
+  add_foreign_key "bookings", "mopeds"
   add_foreign_key "bookings", "users"
   add_foreign_key "mopeds", "users"
 end
